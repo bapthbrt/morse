8a6382e0f6d891d7f169c896a59be9b92d4d94ea
Baptiste Hebert <baptiste.hebert@gmail.com>
2018-11-27 16:31:16 +0100

    All tests passing git status!

--- morse.py
+++ morse.py
@@ -3,5 +3,47 @@
 
 
 class Morse:
+    ALPHABET = {
+        ".-": "A",
+        "-...": "B",
+        "-.-.": "C",
+        "-..": "D",
+        ".": "E",
+        "..-.": "F",
+        "--.": "G",
+        "....": "H",
+        "..": "I",
+        ".---": "J",
+        "-.-": "K",
+        ".-..": "L",
+        "--": "M",
+        "-.": "N",
+        "---": "O",
+        ".--.": "P",
+        "--.-": "Q",
+        ".-.": "R",
+        "...": "S",
+        "-": "T",
+        "..-": "U",
+        "...-": "V",
+        ".--": "W",
+        "-..-": "X",
+        "-.--": "Y",
+        "--..": "Z"
+
+    }
+
     def decode(self, message):
-        pass # TODO: implement the behavior!
+        return_string = ""
+        letter_list = message.split(" ")
+        try:
+            for letter in letter_list:
+                print(letter)
+                return_string += self.ALPHABET[letter]
+        except KeyError:
+            return ""
+        return return_string
+
+
+# if __name__ == "__main__":
+#     print(Morse().decode(".-"))
